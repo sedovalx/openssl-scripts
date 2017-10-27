77c2fbf84834344e9ef571a9265961adb23efb9b
Alexander Sedov <alexander.sedov@jetbrains.com>
2017-10-27 16:49:26 +0300

    Updated the CA script accordingly to the naming scheme

--- ca/bin/create-ca-cert.sh
+++ ca/bin/create-ca-cert.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export OPENSSL_CONF=confs/ca.cnf
-openssl req -x509 -newkey rsa:2048 -out certs/cacert.pem -outform PEM -days 1825
+openssl req -x509 -newkey rsa:2048 -out certs/ca_crt.pem -outform PEM -days 1825
 
-echo "CA certificate is created: certs/cacert.pem, private/cakey.pem"
+echo "CA certificate is created: certs/ca_crt.pem, private/ca_key.pem"
