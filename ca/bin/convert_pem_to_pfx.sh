b7d9d1cbc99b6364979e4dbdf3f67c59c62a68ba
Alexander Sedov <alexander.sedov@jetbrains.com>
2018-04-03 16:42:36 +0300

    Removed the old implementation

--- ca/bin/convert_pem_to_pfx.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cert_name=$1
-desc=$2
-cat private/${cert_name}_key.pem certs/${cert_name}_crt.pem > hold.pem
-openssl pkcs12 -export -out certs/$cert_name.pfx -in hold.pem -name "$desc"
-rm hold.pem
--- ca/bin/create-ca-cert.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export OPENSSL_CONF=confs/ca.cnf
-openssl req -x509 -newkey rsa:2048 -out certs/ca_crt.pem -outform PEM -days 1825
-
-echo "CA certificate is created: certs/ca_crt.pem, private/ca_key.pem"
